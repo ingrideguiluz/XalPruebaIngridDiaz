26fc641bc8c7bf9de1680cb4ca66a93801c4484b
Ingrid <55422795+ingrideguiluz@users.noreply.github.com>
2021-11-24 22:03:36 -0600

    Creacion de metodo de respuestas contestadas y no contestadas

--- AppPython.py
+++ AppPython.py
@@ -9,3 +9,18 @@ def muestra_url(url):
     #print('INSUMO -->', respuesta)
     return respuesta #Imprime el insumo json
 
+#2. Obtener el número de respuestas contestadas y no contestadas
+def valor_de_respuestas():
+    response = requests.get(url)
+    data: dict = response.json()
+
+    contador_true: int = 0
+    contador_false: int = 0
+    for element in data["items"]:
+        respuestas = element["is_answered"]
+        if respuestas == True:
+            contador_true = contador_true + 1
+        elif respuestas == False:
+            contador_false = contador_false + 1
+    print(f"numero de respuestas contestadas: {contador_true}")
+    print|(f"numero de respuestas no contestadas: {contador_false}")            
