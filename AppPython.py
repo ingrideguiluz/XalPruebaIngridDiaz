db7ade24f2e2a49427386d163270b072f03cb7b0
Ingrid <55422795+ingrideguiluz@users.noreply.github.com>
2021-11-24 21:55:49 -0600

    Creación de la conexión al enlace

--- AppPython.py
+++ AppPython.py
@@ -1,3 +1,11 @@
 import requests
 
 url="https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow"
+
+#1. Conectarse al enlace
+def muestra_url(url):
+    response = requests.get(url)
+    respuesta = response.json()
+    #print('INSUMO -->', respuesta)
+    return respuesta #Imprime el insumo json
+
